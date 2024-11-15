d8c2aaef71aac75a7aec1f375ffc9aedb89944b7
freedanjeremiah <freedanjeremiah@gmail.com>
2024-11-15 12:38:09 +0530

    feat - landing page

--- app/page.js
+++ app/page.js
@@ -1,132 +1,148 @@
-// Home.js
-"use client";
-import React, { useState } from "react";
-import axios from "axios";
-import { Button } from "@/components/ui/button";
-import { FaArrowRight } from "react-icons/fa";
-import "./test.css";
+import React from "react";
 
-const Home = () => {
-  const [formData, setFormData] = useState({
-    prompt: "",
-    choiceOfOperator: "",
-    fidelityScore: "",
-  });
-  const [loading, setLoading] = useState(false);
-  const [fromLocation, setFromLocation] = useState(null);
-  const [toLocation, setToLocation] = useState(null);
-
-  const locations = [
-    { name: "Location 1", img: "/location1.png", locationID: "1" },
-    { name: "Location 2", img: "/location2.png", locationID: "2" },
-    { name: "Location 3", img: "/location3.png", locationID: "3" },
-  ];
-
-  const handleChange = (name, value) => {
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleOnDropFromLocation = (e) => {
-    e.preventDefault();
-    const location = JSON.parse(e.dataTransfer.getData("location"));
-    setFromLocation(location);
-  };
-
-  const handleOnDropToLocation = (e) => {
-    e.preventDefault();
-    const location = JSON.parse(e.dataTransfer.getData("location"));
-    setToLocation(location);
-  };
-
-  const handleDragOver = (e) => e.preventDefault();
-
-  const getCircuit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    try {
-      const circuitResponse = await axios.post(
-        "http://127.0.0.1:8080/circuitBot",
-        formData
-      );
-      console.log(circuitResponse.data);
-    } catch (error) {
-      console.error("Error fetching circuit data:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
+const page = () => {
   return (
-    <div className="bg-gray-50 flex flex-col justify-center h-[99vh] px-8 rounded-md">
-      <div className="flex justify-between">
-        <p>Supplier ID:</p>
-        <p>Customer ID:</p>
+    <div className="px-24 pt-[120px] overflow-x-hidden">
+      <div className="h-[65vh]">
+        <div className="flex justify-center gap-[20px]  mt-[0px]  items-center">
+          <div className="flex flex-col ">
+            <p className="text-[50px] font-bold w-[650px] ">
+              Ripples
+            </p>
+
+            <p className="text-[16px] mt-5 mb-10 font-light  w-[600px] tracking-wide">
+              A <span className="text-purple-600 font-semibold">platform</span>{" "}
+              for designing, optimizing, and visualizing quantum circuits with advanced 
+              machine learning and cutting-edge algorithms
+              
+            </p>
+            <div className="text-lg font-semibold flex gap-10">
+              <p>
+                <span className="w-[10px] h-[10px] inline-block border-2 border-solid rounded-full mr-3 bg-purple-500 border-purple-500"></span>
+                Simulation
+              </p>
+              <p>
+                <span className="w-[10px] h-[10px] inline-block border-2 border-solid rounded-full mr-3 bg-purple-500 border-purple-500"></span>
+                Optimization
+              </p>
+              <p>
+                <span className="w-[10px] h-[10px] inline-block border-2 border-solid rounded-full mr-3 bg-purple-500 border-purple-500"></span>
+                Efficiency
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center relative w-fit">
+            <img
+              src="/stick1.png"
+              alt=""
+              className="object-contain h-[400px] w-[400px] joystick"
+            />
+
+            <div class="circles ">
+              <div class="circle1"></div>
+              <div class="circle2"></div>
+              <div class="circle3"></div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="flex justify-between items-center gap-8">
-        <div
-          onDrop={handleOnDropFromLocation}
-          onDragOver={handleDragOver}
-          className="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg"
-        >
-          <h3 className="text-xl font-semibold mb-3 text-gray-700">
-            From Location
-          </h3>
-          {fromLocation ? (
-            <div className="flex flex-col items-center border p-4 rounded-lg h-64 w-56 shadow-md">
-              <p className="font-semibold text-lg text-gray-900">
-                {fromLocation.name}
-              </p>
-              <img
-                className="rounded mt-4"
-                height={150}
-                width={150}
-                src={fromLocation.img}
-                alt={fromLocation.name}
-              />
-            </div>
-          ) : (
-            <div className="flex flex-col items-center justify-center border p-4 rounded-lg h-64 w-56 text-gray-500">
-              <p>Drop From Location here</p>
-            </div>
-          )}
+
+      <div className="flex flex-col  mt-[0px] mb-32 h-[80vh] pt-[12vh]">
+      <p className="text-[50px] font-bold text-center mb-5">
+          Overview 
+        </p>
+        <div className="flex justify-center">
+          <p className="text-xl text-center font-light mb-[80px] w-[60vw]">
+            1. design
+            2. optimization
+            3. Insights
+            4. analysis 
+          </p>
         </div>
-        <Button
-          className="bg-red-500 text-white p-4 rounded-full hover:bg-red-600"
-          disabled={loading}
-          onClick={getCircuit}
-        >
-          {loading ? "Loading..." : <FaArrowRight size={20} />}
-        </Button>
-        <div
-          onDrop={handleOnDropToLocation}
-          onDragOver={handleDragOver}
-          className="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg"
-        >
-          <h3 className="text-xl font-semibold mb-3 text-gray-700">
-            To Location
-          </h3>
-          {toLocation ? (
-            <div className="flex flex-col items-center border p-4 rounded-lg h-64 w-56 shadow-md">
-              <p className="font-semibold text-lg text-gray-900">
-                {toLocation.name}
-              </p>
-              <img
-                className="rounded mt-4"
-                height={150}
-                width={150}
-                src={toLocation.img}
-                alt={toLocation.name}
-              />
-            </div>
-          ) : (
-            <div className="flex flex-col items-center justify-center border p-4 rounded-lg h-64 w-56 text-gray-500">
-              <p>Drop To Location here</p>
-            </div>
-          )}
+       
+        <p className="text-[50px] font-bold text-center mb-5">
+        System Design 
+        </p>
+        <div className="flex justify-center">
+          <p className="text-xl text-center font-light mb-[80px] w-[60vw]">
+            Design image
+            <div className="flex flex-col justify-center items-center relative w-fit">
+            <img
+              src="/stick1.png"
+              alt=""
+              className="object-contain h-[400px] w-[400px] joystick"
+            /></div> 
+          </p>
         </div>
+        
+        <p className="text-[50px] font-bold text-center mb-5">
+          Tech stack 
+        </p>
+        <div className="flex justify-center">
+          <p className="text-xl text-center font-light mb-[80px] w-[60vw]">
+            Next js 
+            python
+            criq 
+          </p>
+          {/* 
+      <div>
+        <Sponsers />
+      </div> */}
+        </div>
+       
+        <p className="text-[50px] font-bold text-center mb-5">
+          Workflow 
+        </p>
+        <div className="flex justify-center">
+          <p className="text-xl text-center font-light mb-[80px] w-[60vw]">
+            image
+            <div className="flex flex-col justify-center items-center relative w-fit">
+            <img
+              src="/stick1.png"
+              alt=""
+              className="object-contain h-[400px] w-[400px] joystick"
+            /></div> 
+          </p>
+        </div>
+        <p className="text-[50px] font-bold text-center mb-5">
+          Use cases 
+        </p>
+        <div className="flex justify-center">
+          <p className="text-xl text-center font-light mb-[80px] w-[60vw]">
+            Through 
+          </p>
+        </div>
+
+        {/* 
+      <div>
+        <Sponsers />
+      </div> */}
+        
+        
+
+
+        {/* <div className="grid grid-cols-4 gap-x-10 px-5">
+          <ServiceCard
+            title="Global Insights"
+            desc="Get a worldwide perspective on decentralized gaming developments."
+          />
+          <ServiceCard
+            title="Deep Dive Articles"
+            desc="In-depth articles covering every aspect of web3 gaming."
+          />
+
+          <ServiceCard
+            title="Exclusive Interviews"
+            desc="Hear from industry leaders and innovators shaping the future of gaming."
+          />
+          <ServiceCard
+            title="Community Hub"
+            desc="Connect with like-minded enthusiasts in our vibrant community."
+          />
+        </div> */}
       </div>
     </div>
   );
 };
 
-export default Home;
+export default page;
